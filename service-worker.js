c8be92e79dea6819b4204a68f661f799adad3523
katkay <136579076+katkay@users.noreply.github.com>
2024-09-04 07:24:58 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,23 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('sticky-notes-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/styles.css',
+                '/app.js',
+                '/manifest.json',
+                '/images/icon-192x192.png',
+                '/images/icon-512x512.png'
+            ]);
+        })
+    );
+});
 
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
